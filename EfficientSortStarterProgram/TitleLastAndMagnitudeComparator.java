b77574ac3db61b62393588909548066afa94b7ea
fendiland <111477989+fendiland@users.noreply.github.com>
2022-11-15 18:55:12 -0600

    Add files via upload

---
+++ EfficientSortStarterProgram/TitleLastAndMagnitudeComparator.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of TitleLastAndMagnitudeComparator here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.util.*;
+public class TitleLastAndMagnitudeComparator implements Comparator<QuakeEntry> {
+
+    public int compare(QuakeEntry q1, QuakeEntry q2) {
+        String title1 = q1.getInfo().substring(q1.getInfo().lastIndexOf(" ")+1);
+        String title2 = q2.getInfo().substring(q2.getInfo().lastIndexOf(" ")+1);
+        if(title1.compareTo(title2) < 0) {
+            return -1;
+        }
+        if(title1.compareTo(title2) > 0) {
+            return 1;
+        } else {
+            return Double.compare(q1.getMagnitude(), q2.getMagnitude());
+        }
+        
+        
+    }
+}
